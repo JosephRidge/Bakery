f171158768c9b67e011750cbcbad66e3bc7a5221
JosephRidge <josephridge18@gmail.com>
2025-05-28 15:44:31 +0300

    update

--- base/forms.py
+++ base/forms.py
@@ -6,6 +6,11 @@ class PostForm(ModelForm):
         model = Post
         fields =['title','content']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["title"].widget.attrs.update({"class": "form-control item"}) 
+        self.fields["content"].widget.attrs.update({"class": "form-control item"}) 
+
 class AuthorForm(ModelForm):
     class Meta:
         model = Author
@@ -21,6 +26,17 @@ class RecipeForm(ModelForm):
     class Meta:  
         model = Recipe
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["decription"].widget.attrs.update({"class": "form-control item h-50"}) 
+        self.fields["name"].widget.attrs.update({"class": "form-control item"}) 
+        self.fields["prepTime"].widget.attrs.update({"class": "form-control item"}) 
+        self.fields["cookTime"].widget.attrs.update({"class": "form-control item"}) 
+        self.fields["level"].widget.attrs.update({"class": "form-control item"}) 
+        self.fields["ingredients"].widget.attrs.update({"class": "form-control item h-50"}) 
+        self.fields["steps"].widget.attrs.update({"class": "form-control item  h-50"}) 
+        self.fields["price"].widget.attrs.update({"class": "form-control item"})  
 
 
 class ShopForm(ModelForm):
